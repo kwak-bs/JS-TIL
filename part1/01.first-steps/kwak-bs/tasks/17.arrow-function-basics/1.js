d57551fdcc25cf62536ea11841501fdd8a033514
kwak-bs <iam-bs@naver.com>
2020-12-27 21:22:33 +0900

    ADD : 진도 2020.12.27

--- part1/01.first-steps/kwak-bs/tasks/17.arrow-function-basics/1.js
+++ part1/01.first-steps/kwak-bs/tasks/17.arrow-function-basics/1.js
@@ -2,7 +2,6 @@
 
 // 함수 표현식을 사용해 만든 아래 함수를 화살표 함수로 바꿔보세요.
 
-
 //function ask(question, yes, no) {
 //  if (confirm(question)) yes()
 //  else no();
@@ -16,15 +15,15 @@
 // 1.
 
 let ask = (question, yes, no) => {
-  if(confirm(question)) {
+  if (confirm(question)) {
     yes();
   } else {
     no();
   }
-}
+};
 
 ask(
   "동의하십니까?",
- () => alert("동의하셨습니다."),
-() => alert("취소 버튼을 누르셨습니다.")
+  () => alert("동의하셨습니다."),
+  () => alert("취소 버튼을 누르셨습니다.")
 );
