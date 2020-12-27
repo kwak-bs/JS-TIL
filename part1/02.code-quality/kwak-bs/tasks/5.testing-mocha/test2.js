f136893da65d8adecd16058ea0c66ed7b259ed8c
kwak-bs <iam-bs@naver.com>
2020-12-28 06:04:26 +0900

    ADD : test2.js 추가 2020.12.28

---
+++ part1/02.code-quality/kwak-bs/tasks/5.testing-mocha/test2.js
@@ -0,0 +1,28 @@
+describe("pow", function() {
+
+  describe("x를 세 번 곱합니다.", function() {
+
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x}을/를 세 번 곱하면 ${expected}입니다.`, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+
+  });
+    
+  it("n이 음수일 때 결과는 NaN입니다.", function() {
+    assert.isNaN(pow(2, -1));
+  });
+
+  it("n이 정수가 아닐 때 결과는 NaN입니다.", function() {
+    assert.isNaN(pow(2, 1.5));
+  });
+
+
+  // describe와 it을 사용해 이 아래에 더 많은 테스트를 추가할 수 있습니다.
+});
